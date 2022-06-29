d4e1987cfa19ae68c985ae7886aaaef42b40eebd
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-06-29 15:44:14 +0300

    commit tree c4bbee364a7526105fb3de5c71c7234201bbdfd9

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your code here
+class Dog {
+    constructor(name, breed){
+        this.name = name;
+        this.breed = breed;
+    }
+}
+let bigDog1 = new Dog("David", "German");
+let bigDog2 = new Dog("Luke", "labrodor");
+
+console.log(bigDog1);
+console.log(bigDog2);
+
+//BreakFast
+class Breakfast {
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+let breakfast1 = new Breakfast("egg", "Juice")
+console.log(breakfast1)
+
+
+//Lunch
+class Lunch {
+    constructor(salad, soup, drink){
+        this.soup = soup;
+        this.drink = drink;
+        this.salad = salad
+    }
+}
+let lunch1 = new Lunch("Veg", "Fish", "Juice")
+console.log(lunch1)
+
+
+//Dinner
+class Dinner {
+    #dessert;
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+}
+let dinner1 = new Dinner("Veg", "Mushroom", "Meat", "Cake")
+console.log(dinner1);
